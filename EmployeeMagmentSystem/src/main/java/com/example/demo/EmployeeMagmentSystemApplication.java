87e7c7dd7bf76719cfcc696ba994761c3a678cbc
Divye Verma <your-email@example.com>
2026-05-25 15:29:18 +0530

    Fixed Docker🏠

--- EmployeeMagmentSystem/src/main/java/com/example/demo/EmployeeMagmentSystemApplication.java
+++ EmployeeMagmentSystem/src/main/java/com/example/demo/EmployeeMagmentSystemApplication.java
@@ -3,10 +3,15 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class EmployeeMagmentSystemApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
 		SpringApplication.run(EmployeeMagmentSystemApplication.class, args);
 	}
 
